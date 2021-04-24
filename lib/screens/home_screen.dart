4b7d0b41330a605b7f5b4100f3939a7fd256908c
Captain Web <contact@captain-web.fr>
2021-04-25 01:03:29 +0200

    dialog

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/controllers/navigation_controller.dart';
-import 'package:my_app/screens/second_screen.dart';
-import 'package:my_app/widgets/app_bar_widget.dart';
-import 'package:my_app/widgets/bottom_navigation_bar_widget.dart';
-import 'package:my_app/widgets/drawer_widget.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -17,67 +12,61 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  NavigationController _appBarController;
-  NavigationController _bottomBarController;
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-  String _fruit = "pomme";
-
-  @override
-  void initState() {
-    super.initState();
-    _appBarController = NavigationController.appBar(scaffoldKey: _scaffoldKey);
-    _bottomBarController = NavigationController.bottomBar();
-  }
+  bool _checked = false;
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBarWidget(appBarController: _appBarController),
-      drawer: DrawerWidget(),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(_fruit),
+              Text("text"),
               SizedBox(height: 20,),
-              PopupMenuButton(
-                initialValue: _fruit,
-                onSelected: (value) {
-                  setState(() {
-                    _fruit = value;
-                  });
-                },
-                itemBuilder: (BuildContext context) {
-                  return [
-                    PopupMenuItem(
-                      value: "pomme",
-                      child: Text("pomme"),
-                    ),
-                    PopupMenuItem(
-                      value: "poire",
-                      child: Text("poire"),
-                    ),
-                    PopupMenuItem(
-                      value: "raisin",
-                      child: Text("raisin"),
-                    ),
-                  ];
-                }
-              )
+              ElevatedButton(
+                  onPressed: () => showDialog(
+                      barrierDismissible: false,
+                      context: context,
+                      builder: (_) => WillPopScope(
+                        onWillPop: () async {
+                          return false;
+                        },
+                        child: StatefulBuilder(
+                          builder: (context, setState) {
+                            return Dialog(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text("titre"),
+                                  Text("ceci est une Dialog"),
+                                  Checkbox(
+                                      value: _checked,
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _checked = value;
+                                        });
+                                      }
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () => Navigator.of(context).pop(),
+                                      child: Text("fermer dialog")
+                                  )
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                  ),
+                  child: Text("open dialog")
+              ),
             ],
           )
-      ), //
-      bottomNavigationBar: BottomNavigationBarWidget(bottomBarController: _bottomBarController,),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("click"),
-        tooltip: "Si tu cliques il va se passer ça",
-        foregroundColor: Colors.white,
-        splashColor: Colors.purple,
-        child: Text("click"),
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
 }
